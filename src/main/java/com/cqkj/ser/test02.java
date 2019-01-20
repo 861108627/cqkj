4668cea0ffbfab7a00ee50e95cfa1509e7c76b79
861108627 <861108627@qq.com>
2019-01-20 12:55:35 +0800

    @version:2.0

---
+++ src/main/java/com/cqkj/ser/test02.java
@@ -0,0 +1,14 @@
+package com.cqkj.ser;
+
+public class test02
+{
+	public void say()
+	{
+		System.out.println("HWLLO WORD!");
+		System.out.println("HWLLO WORD!");
+	}
+	public String sayWord()
+	{
+		return "HWLLO WORD! HWLLO WORD!";
+	}
+}
