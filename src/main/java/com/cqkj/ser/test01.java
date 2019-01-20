59f2b3a7d8da5e58a4bd9de9358a6428061fcbac
861108627 <861108627@qq.com>
2019-01-20 12:54:47 +0800

    @version:2.0

--- src/main/java/com/cqkj/ser/test01.java
+++ src/main/java/com/cqkj/ser/test01.java
@@ -5,9 +5,10 @@ public class test01
 	public void say()
 	{
 		System.out.println("HWLLO WORD!");
+		System.out.println("HWLLO WORD!");
 	}
 	public String sayWord()
 	{
-		return "HWLLO WORD!";
+		return "HWLLO WORD! HWLLO WORD!";
 	}
 }
